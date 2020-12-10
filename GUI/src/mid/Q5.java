e0573f6476ece2a9ec52af9370c66f1d4afcbc31
Osama Khan <Osama6756@gmail.com>
2020-12-10 16:40:37 +0500

    Added Mid - Q5

---
+++ GUI/src/mid/Q5.java
@@ -0,0 +1,70 @@
+package mid;
+
+import javax.swing.*;
+import algos.*;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Q5 extends JFrame {
+	
+	public JPanel panel;
+	private Point selectedPoint;
+	public Q5() {
+		setSize(500, 500);
+		setVisible(true);
+		panel = new JPanel();
+		panel.setSize(500, 500);
+		add(panel);
+		panel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(e.getButton() == MouseEvent.BUTTON1) {
+					selectedPoint = e.getPoint();
+				} else if (e.getButton() == MouseEvent.BUTTON3) {
+					if (selectedPoint != null) {
+						Graphics g = panel.getGraphics();
+						int r = (int) Math.abs(selectedPoint.x - e.getX());
+						midPoint(selectedPoint.x, selectedPoint.y, r, g);
+					}
+				}
+			}
+		});
+	}
+	
+	public static void midPoint(int xc, int yc, int radius, Graphics g)
+	{
+		int x = 0, y = radius;
+		int p = 1 - radius; 
+		octantRule(x, y, xc, yc, g); 
+		while(x<y) { 
+			x++;
+			if(p<0) 
+			{
+				p = p + 2 * x + 1;
+			}
+			else 
+			{
+				y--;
+				p = p + 2 * (x - y) + 1;
+			}
+			octantRule(x, y, xc, yc, g);
+		}
+	}
+
+	public static void octantRule(int x, int y, int xc, int yc, Graphics g) {
+		g.drawString(".", xc + y, yc + x);
+		g.drawString(".", xc + x, yc + y);
+		g.drawString(".", xc - x, yc + y);
+		g.drawString(".", xc - y, yc + x);
+		g.drawString(".", xc - x, yc - y);
+		g.drawString(".", xc - y, yc - x);
+		g.drawString(".", xc + x, yc - y);
+		g.drawString(".", xc + y, yc - x);
+	}
+	
+	public static void main(String[] args) {
+		new Q5();
+	}
+}
