06ba75d6eacde9dbd8b39b205443a8b3560bed28
Osama Khan <Osama6756@gmail.com>
2021-01-10 20:21:22 +0500

    Update Q5.java

--- GUI/src/mid/Q5.java
+++ GUI/src/mid/Q5.java
@@ -25,7 +25,9 @@ public class Q5 extends JFrame {
 				} else if (e.getButton() == MouseEvent.BUTTON3) {
 					if (selectedPoint != null) {
 						Graphics g = panel.getGraphics();
-						int r = (int) Math.abs(selectedPoint.x - e.getX());
+						int xd = (int) Math.abs(selectedPoint.x - e.getX());
+						int yd = (int) Math.abs(selectedPoint.y - e.getY());
+						int r = xd > yd? xd : yd;
 						midPoint(selectedPoint.x, selectedPoint.y, r, g);
 					}
 				}
@@ -33,19 +35,16 @@ public class Q5 extends JFrame {
 		});
 	}
 	
-	public static void midPoint(int xc, int yc, int radius, Graphics g)
-	{
+	public static void midPoint(int xc, int yc, int radius, Graphics g) {
 		int x = 0, y = radius;
 		int p = 1 - radius; 
 		octantRule(x, y, xc, yc, g); 
 		while(x<y) { 
 			x++;
-			if(p<0) 
-			{
+			if(p<0) {
 				p = p + 2 * x + 1;
 			}
-			else 
-			{
+			else {
 				y--;
 				p = p + 2 * (x - y) + 1;
 			}
